ab353938847cb50154f34eceb05af4bcfbe16447
unknown <Fanor@FDN-WS-023537.acad.dvuadmin.net>
2018-01-26 23:14:55 -0200

    init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function parorimpar1 (a){
+     a % 0 ? console.log("PAR") : console.log("ÍMPAR")
+}
+
+const parorimpar = (a) => a % 0 ? console.log("PAR") : console.log("ÍMPAR")
+
+parorimpar(11)
+parorimpar(2)
+parorimpar(5)
